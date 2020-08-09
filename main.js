e5a6cf451c2bee7de837e22d5d83957998179b0c
Zach Dahl <z.schtauffen@gmail.com>
2020-08-09 16:45:36 -0500

    add logo

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-!function() {
+(function () {
   window.addEventListener('load', () => {
-    document.body.style.opacity = 1;
-  });
+    document.body.style.opacity = 1
+  })
 
-  const progress = document.querySelector('#progress');
-  const main = document.querySelector('main');
+  const progress = document.querySelector('#progress')
+  const main = document.querySelector('main')
 
   main.addEventListener('scroll', updateProgress)
-  main.dispatchEvent(new Event('scroll'));
+  main.dispatchEvent(new Event('scroll'))
 
-  function updateProgress(evt) {
-    const { target } = evt;
-    const { scrollTop, clientHeight, scrollHeight } = target;
-    const position = (clientHeight + scrollTop) / scrollHeight; 
-    const width = 100 * bound(0, 1, position);
-    progress.style.width = `${width}%`;
+  function updateProgress (evt) {
+    const { target } = evt
+    const { scrollTop, clientHeight, scrollHeight } = target
+    const position = (clientHeight + scrollTop) / scrollHeight
+    const width = 100 * bound(0, 1, position)
+    progress.style.width = `${width}%`
   }
 
-  function bound(min, max, value) {
-    return Math.max(min, Math.min(max, value));
+  function bound (min, max, value) {
+    return Math.max(min, Math.min(max, value))
   }
-}();
+}())
