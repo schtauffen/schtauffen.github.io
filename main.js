6ce652abb40953de755a86ca7c4dda9dfd421f84
Zach Dahl <zach@grawlix.dev>
2021-10-09 18:34:56 -0500

    darkmode toggle

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 (function () {
+  const IS_DARKMODE_KEY = "@@grawlix/is_darkmode";
+
   window.addEventListener('load', () => {
+    updateNavMenu();
+    updateDarkMode();
     document.body.style.opacity = 1
   })
 
@@ -11,7 +15,9 @@
   const navMenu = document.querySelector('#site-nav');
   navMenu.addEventListener('click', updateNavMenu);
 
-  updateNavMenu();
+  const darkModeToggle = document.querySelector('#dark-mode');
+  darkModeToggle.addEventListener('click', toggleDarkMode);
+
 
   function updateProgress (evt) {
     const { target } = evt
@@ -37,5 +43,49 @@
 
   function bound (min, max, value) {
     return Math.max(min, Math.min(max, value))
+  }
+
+  function getLocalStorageValue(name, def) {
+    let result;
+    try {
+      result = JSON.parse(localStorage.getItem(name));
+      if (result == null) {
+        result = def;
+      }
+    } catch {
+      result = def;
+    }
+    return result;
+  }
+
+  function setLocalStorageValue(name, value) {
+    try {
+      localStorage.setItem(name, JSON.stringify(value));
+    } catch {
+    }
+  }
+
+  function toggleDarkMode() {
+    const isDarkMode = getLocalStorageValue(IS_DARKMODE_KEY, getDefaultDarkMode);
+    setLocalStorageValue(IS_DARKMODE_KEY, !isDarkMode);
+    updateDarkMode();
+  }
+
+  function getDefaultDarkMode() {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? true
+      : false;
+  }
+
+  function updateDarkMode() {
+    const html = document.querySelector('html');
+    const isDarkmode = getLocalStorageValue(IS_DARKMODE_KEY, getDefaultDarkMode);
+    if (isDarkmode) {
+      html.classList.add('dark');
+      darkModeToggle.checked = true;
+    } else {
+      html.classList.remove('dark');
+      darkModeToggle.checked = false;
+    }
   }
 }())
