f1496f521e06d09c3624ed83263930df6d917fbe
Zach Dahl <zach@grawlix.dev>
2021-10-13 23:00:07 -0500

    add recaptcha; form validation

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 (function () {
   const IS_DARKMODE_KEY = "@@grawlix/is_darkmode";
+  const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const email = document.querySelector('#f_7990c819');
+  const message = document.querySelector('#f_83850abb');
 
   window.addEventListener('load', () => {
     updateNavMenu();
     updateDarkMode();
     document.querySelector('.no-script').classList.remove('no-script');
+    document.querySelector('#contactForm').onsubmit = onSubmitContactform;
+    email.onkeyup = validateContactFormEmail;
+    message.onkeyup = validateContactFormMessage;
     document.body.style.opacity = 1
   })
 
@@ -19,6 +26,73 @@
   const darkModeToggle = document.querySelector('#dark-mode');
   darkModeToggle.addEventListener('click', toggleDarkMode);
 
+  function validateContactFormEmail() {
+    if (email.value) {
+      email.classList.add('dirty')
+    } else {
+      email.classList.remove('dirty');
+    }
+
+    if (!EMAIL_REGEX.test(email.value)) {
+      email.classList.add('invalid');
+      return false
+    } else {
+      email.classList.remove('invalid');
+      return true;
+    }
+  }
+
+  function validateContactFormMessage() {
+    if (message.value) {
+      message.classList.add('dirty')
+    } else {
+      message.classList.remove('dirty');
+    }
+
+    if (message.value.trim().length < 5) {
+      message.classList.add('invalid');
+      return false;
+    } else {
+      message.classList.remove('invalid');
+      return true;
+    }
+  }
+
+  function validateContactForm() {
+    let valid = true;
+
+    const honeypot = document.querySelector('#email');
+    const verification = document.querySelector('#f_139a63f4');
+
+    if (honeypot.value || verification.value !== 'manymoonsofhappiness') {
+      valid = false;
+    }
+
+    valid = validateContactFormEmail() && valid;
+    valid = validateContactFormMessage() && valid;
+
+    return valid;
+  }
+
+  function onSubmitContactform(_evt) {
+    if (!validateContactForm()) {
+      return false;
+    }
+
+    // validateAction('contactform', token => {
+      // POST to submit endpoint
+      // THEN if successful:
+      email.value = '';
+      email.classList.remove('dirty');
+      message.value = '';
+      message.classList.remove('dirty');
+      // display "your message was submitted message"
+      // after 8s, delete message
+      // ELSE error message for 8s
+    // });
+
+    return false;
+  }
 
   function updateProgress (evt) {
     const { target } = evt
